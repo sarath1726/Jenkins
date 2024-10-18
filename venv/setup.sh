026a1117d71b9bc8be94a25f3441e3c36b8b6929
Sarath <116967973+sarath1726@users.noreply.github.com>
2024-10-18 17:25:23 +0530

    Update setup.sh

--- venv/setup.sh
+++ venv/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /var/jenkins_home/workspace/My_test/venv/bin/activate  # Adjust the path as necessary
+. /var/jenkins_home/workspace/My_test/venv/bin/activate  # Adjust the path as necessary
 
 python -m ensurepip --upgrade  # Ensure pip is installed
 pip install --upgrade pip  # Upgrade pip
