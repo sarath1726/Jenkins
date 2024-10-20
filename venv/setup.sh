84761d3a8b9512605ba6a6e0872ccbff2834a33f
sarath1726 <sarathas907@gmail.com>
2024-10-20 13:17:05 +0530

    New venv

--- venv/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. /var/jenkins_home/workspace/My_test/venv/bin/activate  # Adjust the path as necessary
-
-python -m ensurepip --upgrade  # Ensure pip is installed
-pip install --upgrade pip  # Upgrade pip
-pip install robotframework pytest  # Install required packages
-pip list  # List installed packages to verify
-
